3dc96cc8b3fdcd26af1b2014f4c74b070e6b104d
dlghks2048 <127286493+dlghks2048@users.noreply.github.com>
2026-02-13 13:34:22 +0900

    Packet Struct create

---
+++ ProjectHeap/GUI_MFC_HeapSorting/GUI_MFC_HeapSorting.h
@@ -0,0 +1,34 @@
+﻿
+// GUI_MFC_HeapSorting.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CGUIMFCHeapSortingApp:
+// 이 클래스의 구현에 대해서는 GUI_MFC_HeapSorting.cpp을(를) 참조하세요.
+//
+
+class CGUIMFCHeapSortingApp : public CWinApp
+{
+public:
+	CGUIMFCHeapSortingApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CGUIMFCHeapSortingApp theApp;
+
+
