a8be31bfea26592f68b549bd085cfa11768da628
dlghks2048 <127286493+dlghks2048@users.noreply.github.com>
2026-02-20 10:51:45 +0900

    헤더파일 pragra once 빠져있었음

--- ProjectHeap/GUI_MFC_HeapSorting/CPacketHeap.h
+++ ProjectHeap/GUI_MFC_HeapSorting/CPacketHeap.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef __CPACKET_HEAP_H__
 #define __CPACKET_HEAP_H__
 
---
+++ ProjectHeap/PakcetServer/sequenceMinHeap.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __CPACKET_HEAP_H__
+#define __CPACKET_HEAP_H__
+
+#include "../Protocol.h"
+#include <windows.h> // CRITICAL_SECTION 사용을 위해 필요(쓰래드)
+
+#define MAX_HEAP_SIZE 1024
+
+// C 컴파일러라고 선언.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef struct {
+        SIM_PACKET heapArray[MAX_HEAP_SIZE];
+        int size;
+        CRITICAL_SECTION cs; // 스레드 동기화용
+    } PacketHeap;
+
+    // 함수 인터페이스
+    void InitHeap(PacketHeap* pHp);
+    void PushHeap(PacketHeap* pHp, SIM_PACKET pkt);
+    int PopHeap(PacketHeap* pHp, SIM_PACKET* pOutPkt); // 성공 시 1, 실패 시 0 반환
+    void DestroyHeap(PacketHeap* pHp);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
