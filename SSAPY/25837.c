f8660a9814262d4f91d4ebb2a9c84d6f07e5838a
dlghks2048 <127286493+dlghks2048@users.noreply.github.com>
2026-03-10 10:21:03 +0900

    25837 SSAPY
    서로 다른 두 자연수라고 명시하지 않았다면 그것은 중근을 허하심을 의미히사니

---
+++ SSAPY/25837.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main () {
+    int num;
+    long long b, c;
+
+    scanf("%d", &num);
+
+    for(int i = 0; i < num; i++){
+        scanf("%lld %lld", &b, &c);
+        
+        int found = 0;
+        for(int N1 = 1; N1*N1 < c;  N1++){
+            if(c%N1 == 0){
+                int N2 = b-N1;
+                if(N2*N1 == c){
+                    found = 1;
+                    break;
+                }
+            }
+        }
+
+        if(found)
+            printf("Yes\n");
+        else 
+            printf("No\n");
+    }
+}
